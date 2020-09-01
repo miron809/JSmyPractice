3ad0425fc8f30a7900e9833fd83ba24a38a2fcfb
Dmytro Myronenko <dim@uain.su>
2020-09-01 18:27:14 +0300

    updated images

--- slider+textLink/index.ts
+++ slider+textLink/index.ts
@@ -1,35 +1,49 @@
 import { Slider } from './slider/slider';
-import animal from './img/animal.jpg';
-import animal2 from './img/animal2.jpg';
-import animal3 from './img/animal3.jpg';
+import animals1 from './img/animals1.jpg';
+import animals2 from './img/animals2.jpg';
+import animals3 from './img/animals3.jpg';
+import animals4 from './img/animals4.jpg';
+import animals5 from './img/animals5.jpg';
 
-import human from './img/human.jpg';
-import human2 from './img/human2.jpg';
-import human3 from './img/human3.jpg';
+import people1 from './img/people1.jpg';
+import people2 from './img/people2.jpg';
+import people3 from './img/people3.jpg';
+import people4 from './img/people4.jpg';
+import people5 from './img/people5.jpg';
 
-import forest from './img/forest.jpg';
-import forest2 from './img/forest2.jpg';
+import music1 from './img/music1.jpg';
+import music2 from './img/music2.jpg';
+import music3 from './img/music3.jpg';
 
-import sea from './img/sea.jpg';
+import education1 from './img/education1.jpg';
+import education2 from './img/education2.jpg';
+import education3 from './img/education3.jpg';
 
 
 const data = {
-  animal: [
-    `<img src=${animal} alt="">`,
-    `<img src=${animal2} alt="">`,
-    `<img src=${animal3} alt="">`,
+  animals: [
+    `<img src=${animals1} alt="">`,
+    `<img src=${animals2} alt="">`,
+    `<img src=${animals3} alt="">`,
+    `<img src=${animals4} alt="">`,
+    `<img src=${animals5} alt="">`,
   ],
-  human: [
-    `<img src=${human} alt="">`,
-    `<img src=${human2} alt="">`,
-    `<img src=${human3} alt="">`,
+  people: [
+    `<img src=${people1} alt="">`,
+    `<img src=${people2} alt="">`,
+    `<img src=${people3} alt="">`,
+    `<img src=${people4} alt="">`,
+    `<img src=${people5} alt="">`,
   ],
-  forest: [
-    `<img src=${forest} alt="">`,
-    `<img src=${forest2} alt="">`,
+  music: [
+    `<img src=${music1} alt="">`,
+    `<img src=${music2} alt="">`,
+    `<img src=${music3} alt="">`,
   ],
-  sea: [
-    `<img src=${sea} alt="">`,
+  education: [
+    `<img src=${education1} alt="">`,
+    `<img src=${education2} alt="">`,
+    `<img src=${education3} alt="">`,
   ]
 }
 
