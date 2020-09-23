781093837eef827a3aab6e837187e1f70e790572
Dmytro Myronenko <dim@uain.su>
2020-09-23 13:00:53 +0300

    updated animations project

--- animations/bubbles/index.js
+++ animations/bubbles/index.js
@@ -6,189 +6,214 @@
  * @class Particles
  * @constructor
  */
-function Particles(){
-  //particle colors
-  this.colors = [
-    '255, 255, 255',
-    '255, 99, 71',
-    '19, 19, 19'
-  ]
-  //adds gradient to particles on true
-  this.blurry = true;
-  //adds white border
-  this.border = false;
-  //particle radius min/max
-  this.minRadius = 10;
-  this.maxRadius = 35;
-  //particle opacity min/max
-  this.minOpacity = .005;
-  this.maxOpacity = .5;
-  //particle speed min/max
-  this.minSpeed = .05;
-  this.maxSpeed = .5;
-  //frames per second
-  this.fps = 60;
-  //number of particles
-  this.numParticles = 75;
-  //required canvas variables
-  this.canvas = document.getElementById('canvas');
-  this.ctx = this.canvas.getContext('2d');
-}
-
-/**
- * Initializes everything
- * @method init
- */
-Particles.prototype.init = function(){
-  this.render();
-  this.createCircle();
-}
-
-/**
- * generates random number between min and max values
- * @param  {number} min value
- * @param  {number} max malue
- * @return {number} random number between min and max
- * @method _rand
- */
-Particles.prototype._rand = function(min, max){
-  return Math.random() * (max - min) + min;
-}
-
-/**
- * Sets canvas size and updates values on resize
- * @method render
- */
-Particles.prototype.render = function(){
-  var self = this,
-    wHeight = canvas.clientHeight,
-    wWidth = canvas.clientWidth;
-
-  self.canvas.width = wWidth;
-  self.canvas.height = wHeight;
-
-  // $(window).on('resize', self.render);
-  window.addEventListener('resize', self.render)
-}
-
-/**
- * Randomly creates particle attributes
- * @method createCircle
- */
-Particles.prototype.createCircle = function(){
-  var particle = [];
-
-  for (var i = 0; i < this.numParticles; i++) {
-    var self = this,
-      color = self.colors[~~(self._rand(0, self.colors.length))];
-
-    particle[i] = {
-      radius    : self._rand(self.minRadius, self.maxRadius),
-      xPos      : self._rand(0, canvas.width),
-      yPos      : self._rand(0, canvas.height),
-      xVelocity : self._rand(self.minSpeed, self.maxSpeed),
-      yVelocity : self._rand(self.minSpeed, self.maxSpeed),
-      color     : 'rgba(' + color + ',' + self._rand(self.minOpacity, self.maxOpacity) + ')'
-    }
-
-    //once values are determined, draw particle on canvas
-    self.draw(particle, i);
-  }
-  //...and once drawn, animate the particle
-  self.animate(particle);
-}
-
-/**
- * Draws particles on canvas
- * @param  {array} Particle array from createCircle method
- * @param  {number} i value from createCircle method
- * @method draw
- */
-Particles.prototype.draw = function(particle, i){
-  var self = this,
-    ctx = self.ctx;
-
-  if (self.blurry === true ) {
-    //creates gradient if blurry === true
-    var grd = ctx.createRadialGradient(particle[i].xPos, particle[i].yPos, particle[i].radius, particle[i].xPos, particle[i].yPos, particle[i].radius/1.25);
-
-    grd.addColorStop(1.000, particle[i].color);
-    grd.addColorStop(0.000, 'rgba(34, 34, 34, 0)');
-    ctx.fillStyle = grd;
-  } else {
-    //otherwise sets to solid color w/ opacity value
-    ctx.fillStyle = particle[i].color;
+class Particles {
+  #defaultOptions = {
+    colors: [
+      '255, 255, 255',
+      '255, 99, 71',
+      '19, 19, 19'
+    ],
+    blurry: true,
+    border: false,
+    minRadius: 10,
+    maxRadius: 35,
+    minOpacity: .005,
+    maxOpacity: .5,
+    minSpeed: .05,
+    maxSpeed: 5,
+    fps: 60,
+    numParticles: 75
   }
 
-  if (self.border === true) {
-    ctx.strokeStyle = '#fff';
-    ctx.stroke();
+  constructor(element = 'canvas', options) {
+    this.options = Object.assign(this.#defaultOptions, options)
+
+    //particle colors
+    this.colors = this.options.colors;
+    //adds gradient to particles on true
+    this.blurry = this.options.blurry;
+    //adds white border
+    this.border = this.options.border;
+    //particle radius min/max
+    this.minRadius = this.options.minRadius;
+    this.maxRadius = this.options.maxRadius;
+    //particle opacity min/max
+    this.minOpacity = this.options.minOpacity;
+    this.maxOpacity = this.options.maxOpacity;
+    //particle speed min/max
+    this.minSpeed = this.options.minSpeed;
+    this.maxSpeed = this.options.maxSpeed;
+    //frames per second
+    this.fps = this.options.fps;
+    //number of particles
+    this.numParticles = this.options.numParticles;
+    //required canvas variables
+    this.canvas = document.getElementById(element);
+    this.ctx = this.canvas.getContext('2d');
   }
 
-  ctx.beginPath();
-  ctx.arc(particle[i].xPos, particle[i].yPos, particle[i].radius, 0, 2 * Math.PI, false);
-  ctx.fill();
-}
 
-/**
- * Animates particles
- * @param  {array} particle value from createCircle & draw methods
- * @method animate
- */
-Particles.prototype.animate = function(particle){
-  var self = this,
-    ctx = self.ctx;
+  /**
+   * Initializes everything
+   * @method init
+   */
+  init() {
+    this.render();
+    this.createCircle();
+  }
 
-  setInterval(function(){
-    //clears canvas
-    self.clearCanvas();
-    //then redraws particles in new positions based on velocity
-    for (var i = 0; i < self.numParticles; i++) {
-      particle[i].xPos += particle[i].xVelocity;
-      particle[i].yPos -= particle[i].yVelocity;
+  /**
+   * generates random number between min and max values
+   * @param  {number} min value
+   * @param  {number} max malue
+   * @return {number} random number between min and max
+   * @method #rand
+   */
+  static rand(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-      //if particle goes off screen call reset method to place it offscreen to the left/bottom
-      if (particle[i].xPos > self.canvas.width + particle[i].radius || particle[i].yPos > self.canvas.height + particle[i].radius) {
-        self.resetParticle(particle, i);
-      } else {
-        self.draw(particle, i);
+  /**
+   * Sets canvas size and updates values on resize
+   * @method render
+   */
+  render() {
+    this.canvas.height = this.canvas.clientHeight;
+    this.canvas.width = this.canvas.clientWidth;
+
+    window.addEventListener('resize', this.render)
+  }
+
+  /**
+   * Randomly creates particle attributes
+   * @method createCircle
+   */
+  createCircle() {
+    const particle = [];
+
+    for (let i = 0; i < this.numParticles; i++) {
+      const color = this.colors[~~(Particles.rand(0, this.colors.length))];
+
+      particle[i] = {
+        radius: Particles.rand(this.minRadius, this.maxRadius),
+        xPos: Particles.rand(0, canvas.width),
+        yPos: Particles.rand(0, canvas.height),
+        xVelocity: Particles.rand(this.minSpeed, this.maxSpeed),
+        yVelocity: Particles.rand(this.minSpeed, this.maxSpeed),
+        color: 'rgba(' + color + ',' + Particles.rand(this.minOpacity, this.maxOpacity) + ')'
       }
+
+      //once values are determined, draw particle on canvas
+      this.draw(particle, i);
     }
-  }, 1000/self.fps);
-}
-
-/**
- * Resets position of particle when it goes off screen
- * @param  {array} particle value from createCircle & draw methods
- * @param  {number} i value from createCircle method
- * @method resetParticle
- */
-Particles.prototype.resetParticle = function(particle, i){
-  var self = this;
-
-  var random = self._rand(0, 1);
-
-  if (random > .5) {
-    // 50% chance particle comes from left side of window...
-    particle[i].xPos = -particle[i].radius;
-    particle[i].yPos = self._rand(0, canvas.height);
-  } else {
-    //... or bottom of window
-    particle[i].xPos = self._rand(0, canvas.width);
-    particle[i].yPos = canvas.height + particle[i].radius;
+    //...and once drawn, animate the particle
+    this.animate(particle);
   }
-  //redraw particle with new values
-  self.draw(particle, i);
-}
 
-/**
- * Clears canvas between animation frames
- * @method clearCanvas
- */
-Particles.prototype.clearCanvas = function(){
-  this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+  /**
+   * Draws particles on canvas
+   * @param  {array} Particle array from createCircle method
+   * @param  {number} i value from createCircle method
+   * @method draw
+   */
+  draw(particle, i) {
+    if (this.blurry === true) {
+      //creates gradient if blurry === true
+      const grd = this.ctx.createRadialGradient(particle[i].xPos, particle[i].yPos, particle[i].radius, particle[i].xPos, particle[i].yPos, particle[i].radius / 1.25);
+
+      grd.addColorStop(1.000, particle[i].color);
+      grd.addColorStop(0.000, 'rgba(34, 34, 34, 0)');
+      this.ctx.fillStyle = grd;
+    } else {
+      //otherwise sets to solid color w/ opacity value
+      this.ctx.fillStyle = particle[i].color;
+    }
+
+    if (this.border) {
+      this.ctx.strokeStyle = '#fff';
+      this.ctx.stroke();
+    }
+
+    this.ctx.beginPath();
+    this.ctx.arc(particle[i].xPos, particle[i].yPos, particle[i].radius, 0, 2 * Math.PI, false);
+    this.ctx.fill();
+  }
+
+  /**
+   * Animates particles
+   * @param  {array} particle value from createCircle & draw methods
+   * @method animate
+   */
+  animate(particle) {
+    setInterval(() => {
+      //clears canvas
+      this.clearCanvas();
+      //then redraws particles in new positions based on velocity
+      for (let i = 0; i < this.numParticles; i++) {
+        particle[i].xPos += particle[i].xVelocity;
+        particle[i].yPos -= particle[i].yVelocity;
+
+        //if particle goes off screen call reset method to place it offscreen to the left/bottom
+        if (particle[i].xPos > this.canvas.width + particle[i].radius || particle[i].yPos > this.canvas.height + particle[i].radius) {
+          this.resetParticle(particle, i);
+        } else {
+          this.draw(particle, i);
+        }
+      }
+    }, 1000 / this.fps);
+  }
+
+  /**
+   * Resets position of particle when it goes off screen
+   * @param  {array} particle value from createCircle & draw methods
+   * @param  {number} i value from createCircle method
+   * @method resetParticle
+   */
+  resetParticle(particle, i) {
+    const random = Particles.rand(0, 1);
+
+    if (random > .5) {
+      // 50% chance particle comes from left side of window...
+      particle[i].xPos = -particle[i].radius;
+      particle[i].yPos = Particles.rand(0, this.canvas.height);
+    } else {
+      //... or bottom of window
+      particle[i].xPos = Particles.rand(0, this.canvas.width);
+      particle[i].yPos = this.canvas.height + particle[i].radius;
+    }
+    //redraw particle with new values
+    this.draw(particle, i);
+  }
+
+  /**
+   * Clears canvas between animation frames
+   * @method clearCanvas
+   */
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
 }
 
 
 // go go go!
-var particle = new Particles().init();
+const options = {
+  colors: [
+    '255, 255, 255',
+    '0, 255, 255',
+    '255, 255, 0',
+    '255, 0, 0',
+  ],
+  blurry: true,
+  border: false,
+  minRadius: 10,
+  maxRadius: 35,
+  minOpacity: .005,
+  maxOpacity: .5,
+  minSpeed: .05,
+  maxSpeed: 5,
+  fps: 60,
+  numParticles: 100
+}
+
+const particle = new Particles('canvas', options).init();
